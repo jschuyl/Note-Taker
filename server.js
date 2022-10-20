4475c37b5e685f263975cd07ac92419f79585ef5
jschuyl <schuylertjed@gmail.com>
2022-10-19 18:20:36 -0700

    trying to get rid of the develop folder, but page isn't updating when I click on things anymore

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 // these are my npms, there are many like them but these are mine
-const { notStrictEqual } = require("assert");
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
@@ -7,34 +6,18 @@ const path = require("path");
 const app = express();
 // Heroku and local port access
 const PORT = process.env.PORT || 3001;
-
 // parse set up
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static("public"));
 
-fs.readFile("./db/db.json", "utf-8", (err, data) => {
-    if (err) throw err;
-    var note = JSON.parse(data);
-    // get route for the notes
-    app.get("/api/notes", (req, res) => {
-        res.json(note);
-    });
-    // post route for the notes
-    app.post("./api/notes", function(req, res) {
-        let newNote = req.body;
-        note.push(newNote);
-        doDbThings();
-    })
-    // database updater
-    function doDbThings() {
-        fs.writeFile("./db/db.json", JSON.stringify(note, "\t"), err => {
-            if (err) throw err;
-            return true;
-        })
-    }
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
 })
 
+app.get("*", (req, res) => 
+res.sendFile(path.join(__dirname, "/public/index.html"))
+);
 
 app.listen(PORT, () =>
   console.log("Listening at http://localhost:" + PORT)
