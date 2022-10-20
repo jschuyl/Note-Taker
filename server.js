1715857255c0d3682d8af102c41b0ca261c3b858
jschuyl <schuylertjed@gmail.com>
2022-10-19 18:47:37 -0700

    I can't get things to connect. it depolys, but I can't test it because the given code doesn't work

--- server.js
+++ server.js
@@ -11,9 +11,18 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 
+let note = JSON.parse(data);
+
+// sends notes
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"))
-})
+});
+// posts notes
+app.post("/api/notes", (req, res) => {
+  let newNote = req.body;
+  note.push(newNote);
+  doDbThings();
+});
 
 app.get("*", (req, res) => 
 res.sendFile(path.join(__dirname, "/public/index.html"))
@@ -22,3 +31,10 @@ res.sendFile(path.join(__dirname, "/public/index.html"))
 app.listen(PORT, () =>
   console.log("Listening at http://localhost:" + PORT)
 );
+
+function doDbThings() {
+  fs.writeFile("/db/db.json", JSON.stringify(note, "\t"), err => {
+    if (err) throw err;
+    return true;
+  });
+}
