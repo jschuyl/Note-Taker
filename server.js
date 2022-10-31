f55f7ff3fe9a0969759760f93795a3a315db3cc2
jschuyl <schuylertjed@gmail.com>
2022-10-31 11:10:48 -0700

    pushing to access on another pc

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+// creates random id
+const uuid = require("uuid");
 // easy word to make express more better to use
 const app = express();
 // Heroku and local port access
@@ -11,20 +13,44 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 
-let note = JSON.parse(data);
-
+const writeToDb = (destination, content) =>{
+  fs.writeFile(destination, JSON.stringify(content, null, "\t"), err => {
+    if (err) throw err;
+    return true;
+  })
+}
+const addWhatYouRead = (content, file) => {
+  fs.readFile(file, "utf-8", (err, data) => {
+    if (err) {
+      console.error(err)
+    } else {
+      const note = JSON.parse(data);
+      note.push(content);
+      writeToDb();
+    }
+  }
+}
 // sends notes
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"))
 });
 // posts notes
 app.post("/api/notes", (req, res) => {
-  let newNote = req.body;
-  note.push(newNote);
-  doDbThings();
+  const { title, text } = req.body;
+  if (req.boy) {
+    const newNote = {
+      title,
+      text,
+      id: uuid.v4()
+    }
+    addWhatYouRead(newNote, "./db/db.json")
+    res.json(newNote);
+  } else {
+    res.error("Note won't write")
+  }
 });
 
-app.get("*", (req, res) => 
+app.get("/", (req, res) => 
 res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
@@ -32,9 +58,9 @@ app.listen(PORT, () =>
   console.log("Listening at http://localhost:" + PORT)
 );
 
-function doDbThings() {
+/*function doDbThings() {
   fs.writeFile("/db/db.json", JSON.stringify(note, "\t"), err => {
     if (err) throw err;
     return true;
   });
-}
+}*/
